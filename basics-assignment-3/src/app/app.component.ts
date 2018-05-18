0a64d46ea2982d92fad6dcfd3b94a5fb5e717708
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-18 11:41:57 +0200

    Toggle displaying paragraph

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  secretHidden = false;
+
+  onButtonClick() {
+    this.secretHidden = !this.secretHidden;
+  }
 }
