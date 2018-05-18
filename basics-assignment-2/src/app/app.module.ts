6496bd7e7c14351aec3c813d683d5f7844e39a8c
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-18 09:07:38 +0200

    Add username field with binded property

--- basics-assignment-2/src/app/app.module.ts
+++ basics-assignment-2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UsernameFormComponent } from './username-form/username-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsernameFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ basics-assignment-2/src/app/username-form/username-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username-form',
+  templateUrl: './username-form.component.html',
+  styleUrls: ['./username-form.component.css']
+})
+export class UsernameFormComponent implements OnInit {
+  username = '';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
