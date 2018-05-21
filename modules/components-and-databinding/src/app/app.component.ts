31f3faa31ea2a90c0f8f33607cf7abf246b71572
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-21 08:24:01 +0200

    Add output binding

--- modules/components-and-databinding/src/app/app.component.ts
+++ modules/components-and-databinding/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{ type: 'server', name: 'server name', content: 'just a test content' }];
+  
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
