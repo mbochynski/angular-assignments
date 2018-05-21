097ae8a4c96fc21d30d2ebbd2a578e23299a63d5
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-21 10:20:23 +0200

    Add game control tick event emitter

--- cmp-databinding-assignment/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() tick = new EventEmitter<number>();
+
+  isGameStarted = false;
+  timer = null;
+  tickCounter = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartClicked() {
+    this.isGameStarted = true;
+    if (!this.timer) {
+      this.timer = setInterval(() => {
+        this.tick.emit(this.tickCounter);
+        this.tickCounter += 1;
+      }, 1000);
+    }
+  }
+
+  onStopClicked() {
+    clearInterval(this.timer);
+    this.timer = null;
   }
 
 }
