0e919d3a1c97ab30b25a1ad1e1554d0499410548
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-22 08:38:40 +0200

    Add counter service and inject it into users service

--- services-assignment/src/app/counter.service.ts
+++ services-assignment/src/app/counter.service.ts
@@ -1,4 +1,16 @@
 export class CounterService {
+  activeInactiveTransitions = 0;
+  inactiveActiveTransitions = 0;
 
   constructor() { }
+
+  logActiveToInactiveTransition() {
+    this.activeInactiveTransitions += 1;
+    console.log(`active -> inactive transitions number: ${this.activeInactiveTransitions}`);
+  }
+
+  logInactiveToActiveTransition() {
+    this.inactiveActiveTransitions += 1;
+    console.log(`inactive -> active transitions number: ${this.inactiveActiveTransitions}`);
+  }
 }
