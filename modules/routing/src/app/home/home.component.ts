1da2fec08028537c13ad0b7bf7eb55389b2f1a0a
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-23 10:51:06 +0200

    Make use of query params and fragment

--- modules/routing/src/app/home/home.component.ts
+++ modules/routing/src/app/home/home.component.ts
@@ -13,9 +13,14 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     //calculations
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: {
+        allowEdit: 1,
+      },
+      fragment: 'loading',
+    });
   }
 
 }
