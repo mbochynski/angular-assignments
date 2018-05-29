51fb7298609e86454532ba4783aa709872e22ec0
Mateusz Bochynski <matbochynski@gmail.com>
2018-05-29 08:57:21 +0200

    Provide default value to input

--- modules/forms-template-driven/src/app/app.component.ts
+++ modules/forms-template-driven/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
   @ViewChild('f') myForm: NgForm;
 
   suggestUserName() {
